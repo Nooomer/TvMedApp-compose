856e3619b3b16ffea1773e060ccce86d36fd9895
Nooomer <ampilogov.qaz@gmail.com>
2022-12-05 12:39:14 +0500

    Using TreatmentViewModel and Flow

--- app/src/main/java/com/example/tvmedapp_compose/TreatmentActivity.kt
+++ app/src/main/java/com/example/tvmedapp_compose/TreatmentActivity.kt
@@ -1,52 +1,145 @@
 package com.example.tvmedapp_compose
 
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.foundation.*
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.*
+import androidx.compose.ui.*
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.*
+import com.example.tvmedapp_compose.interfaces.*
+import com.example.tvmedapp_compose.models.*
 import com.example.tvmedapp_compose.ui.theme.TvMedApp_composeTheme
+import kotlinx.coroutines.*
 
-class TreatmentActivity : ComponentActivity() {
+private fun <T> CoroutineScope.asyncIO(ioFun: () -> T) = async(Dispatchers.IO) { ioFun() }
+private var result: List<TreatmentModel?>? = null
+private var result2: List<UserModel?>? = null
+private val scope = CoroutineScope(Dispatchers.Main + Job())
+private var viewSize: Int = 0
+var data = mutableListOf<MutableList<String?>>()
+class TreatmentActivity : ComponentActivity(), PreferenceDataType, RetrorfitFun {
+    private lateinit var mContext: Context
+    val treatmentFlow = TreatmentModelView().treatmentFlow
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //load()
         setContent {
             TvMedApp_composeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting2("Android")
-
+                ) { // the key define when the block is relaunched // Your coroutine code her
+                    mContext = LocalContext.current
+                    Greeting2(mContext)
                 }
             }
         }
     }
-}
 
-@OptIn(ExperimentalFoundationApi::class, ExperimentalMaterial3Api::class,
-    ExperimentalMaterialApi::class
+    override fun onRestart() {
+        super.onRestart()
+    }
+
+    /*fun load(){
+        when (ssm.fetch(USER_TYPE)) {
+            "doctor" ->
+            {
+                scope.launch {
+                    val def = scope.asyncIO {
+                        result =
+                            get(
+                                "all",
+                                "treatment","Bearer "+ssm.fetch(USER_TOKEN)
+                            ) as List<TreatmentModel?>?
+                    }
+                    def.await()
+                    try {
+                        viewSize = result!!.size
+                        for (i in 0 until viewSize) {
+                            data.add(
+                                mutableListOf<String?>(
+                                    result!![i]?.patientSurename,
+                                    result!![i]?.doctorSurname,
+                                    result!![i]?.startdate
+                                )
+                            )
+
+                        }
+                    }
+                    catch (e: NullPointerException){
+                        val toast = Toast.makeText(
+                            applicationContext,
+                            "Данные не загружены",
+                            Toast.LENGTH_SHORT
+                        )
+                        toast.show()
+                    }
+                }
+            }
+            "patient"->{
+                scope.launch {
+                    val def = scope.asyncIO {
+                        result =
+                            get(
+                                "filtered",
+                                "treatment", "Bearer " + ssm.fetch(USER_TOKEN), ssm.fetch(USER_ID)!!
+                            ) as List<TreatmentModel?>?
+                    }
+                    def.await()
+                }
+                    try {
+                        viewSize = result!!.size
+                    }
+                    catch (e: NullPointerException){
+                        val toast = Toast.makeText(
+                            applicationContext,
+                            "Данные не загружены",
+                            Toast.LENGTH_SHORT
+                        )
+                        toast.show()
+                    }
+                    for (i in 0 until viewSize) {
+                        data.add(
+                            mutableListOf<String?>(
+                                result!![i]?.patientSurename,
+                                result!![i]?.doctorSurname,
+                                result!![i]?.startdate
+                            )
+                        )
+                    }
+            }
+        }
+    }
+}
+*/
+@OptIn(ExperimentalFoundationApi::class
 )
 @Composable
-fun Greeting2(name: String, modifier: Modifier = Modifier) {
+fun Greeting2(context: Context) {
+        var list2 by remember { mutableStateOf<List<TreatmentModel?>?>(listOf(TreatmentModel("Загрузка...","Загрузка...","Загрузка...")))}
+        LaunchedEffect(Unit) {
+            treatmentFlow.collect {
+                list2 = it
+            }
+        }
         LazyColumn(
             modifier = Modifier.fillMaxWidth(),
             verticalArrangement = Arrangement.spacedBy(8.dp),
@@ -61,18 +154,10 @@ fun Greeting2(name: String, modifier: Modifier = Modifier) {
                         .background(Color.Gray),
                     textAlign = TextAlign.Center
                 )
+                LinearProgressIndicator()
             }
-
-            item {
-                ListItem(text = "First item")
-            }
-
-            itemsIndexed(items = (1..10).toList()) { pos, _ ->
-                ListItem("Item $pos")
-            }
-
-            item {
-                ListItem(text = "Last Item")
+            itemsIndexed(items = list2!!) { pos, _ ->
+                ListItem(list2!![pos]?.doctorSurname, list2!![pos]?.patientSurename, list2!![pos]?.startdate)
             }
         }
     Column(
@@ -82,15 +167,20 @@ fun Greeting2(name: String, modifier: Modifier = Modifier) {
     ){
         ExtendedFloatingActionButton(
             onClick = {
-
+                ssm.clearSession()
+                val activity = (context as? Activity)
+                context.startActivity(Intent(context, MainActivity::class.java))
+                activity?.finish()
             }
         ){
 
         }
     }
 }
+
+
 @Composable
-fun ListItem(text: String, modifier: Modifier = Modifier) {
+fun ListItem(doctorSurename: String?, patientSurename: String?, dateOfStart: String?, modifier: Modifier = Modifier) {
     Surface(
         elevation = 8.dp,
         shape = RoundedCornerShape(8.dp),
@@ -107,35 +197,35 @@ fun ListItem(text: String, modifier: Modifier = Modifier) {
                 modifier = Modifier.fillMaxHeight(),
                 horizontalAlignment = Alignment.Start
             ) {
-                Text(
-                    modifier = Modifier
-                        .padding(start = 6.dp),
-                    text = text,
-                    fontSize = 24.sp
-                )
-                Text(
-                    modifier = Modifier
-                        .padding(start = 6.dp),
-                    text = text,
-                    fontSize = 24.sp
-                )
+                if (doctorSurename != null) {
+                    Text(
+                        modifier = Modifier
+                            .padding(start = 6.dp),
+                        text = doctorSurename,
+                        fontSize = 24.sp
+                    )
+                }
+                if (patientSurename != null) {
+                    Text(
+                        modifier = Modifier
+                            .padding(start = 6.dp),
+                        text = patientSurename,
+                        fontSize = 24.sp
+                    )
+                }
             }
             Column(
                 modifier = Modifier.fillMaxHeight(),
                 horizontalAlignment = Alignment.End
             ) {
-                Text(
-                    modifier = Modifier
-                        .padding(end = 6.dp),
-                    text = text,
-                    fontSize = 24.sp
-                )
-                Text(
-                    modifier = Modifier
-                        .padding(end = 6.dp),
-                    text = text,
-                    fontSize = 24.sp
-                )
+                if (dateOfStart != null) {
+                    Text(
+                        modifier = Modifier
+                            .padding(end = 6.dp),
+                        text = dateOfStart,
+                        fontSize = 24.sp
+                    )
+                }
             }
         }
     }
@@ -144,6 +234,7 @@ fun ListItem(text: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview2() {
     TvMedApp_composeTheme {
-        Greeting2("Android")
+        Greeting2(LocalContext.current)
+    }
     }
 }
