ba5f527b01467f53ac9e579106d2cbcb876c4d7f
Nooomer <ampilogov.qaz@gmail.com>
2022-12-05 12:34:45 +0500

    Change host

--- app/src/main/java/com/example/tvmedapp_compose/RetrifitService/Common.kt
+++ app/src/main/java/com/example/tvmedapp_compose/RetrifitService/Common.kt
@@ -3,7 +3,7 @@ package com.example.tvmedapp_compose.RetrifitService
 import com.example.tvmedapp_compose.API
 
 object Common {
-    private const val BASE_URL = "https://7260-83-149-19-254.eu.ngrok.io/"
+    private const val BASE_URL = "https://44a8-79-126-115-109.eu.ngrok.io/"
     val retrofitService: API
         get() = RetrofitClient.getClient(BASE_URL).create(API::class.java)
 }
