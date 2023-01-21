3cb9782ca9edcca28931d98570d48a15145886e4
Nooomer <ampilogov.qaz@gmail.com>
2023-01-21 19:06:34 +0500

    Refactor and tests

--- app/src/androidTest/java/ru/nooomer/tvmedapp_compose/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/ru/nooomer/tvmedapp_compose/ExampleInstrumentedTest.kt
@@ -1,12 +1,21 @@
 package ru.nooomer.tvmedapp_compose
 
+import android.content.Intent
+import androidx.compose.ui.platform.textInputServiceFactory
+import androidx.test.core.app.ActivityScenario
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.rules.activityScenarioRule
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.*
+import org.junit.After
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -19,6 +28,31 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.tvmedapp_compose", appContext.packageName)
+        assertEquals("ru.nooomer.tvmedapp_compose", appContext.packageName)
+    }
+    @Test
+    fun checkButtonDisabled() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+
+        assertEquals("ru.nooomer.tvmedapp_compose", appContext.packageName)
+    }
+}
+@RunWith(AndroidJUnit4::class)
+class MainActivityUiTest {
+    private lateinit var scenario: ActivityScenario<MainActivity>
+    @JvmField
+    @Rule
+    val rule = activityScenarioRule<MainActivity>()
+    @Test
+    fun someTest() {
+        scenario = rule.scenario
+        scenario.onActivity {
+
+        }
+    }
+    @After
+    fun cleanup() {
+        scenario.close()
     }
 }
