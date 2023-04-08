bb8025c3388fbd03c5d4f0fd691cf10b09716374
Nooomer <ampilogov.qaz@gmail.com>
2023-04-09 02:05:09 +0500

    TextField

--- app/src/main/java/ru/nooomer/tvmedapp_compose/ChatActivity.kt
+++ app/src/main/java/ru/nooomer/tvmedapp_compose/ChatActivity.kt
@@ -4,21 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredHeightIn
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Scaffold
+import androidx.compose.material.TextField
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -26,6 +32,7 @@ import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -47,38 +54,67 @@ class ChatActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     //Greeting("Android")
+
                     MessagesScreen(messagesViewModel)
                 }
             }
         }
     }
 }
+
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun MessagesScreen(viewModel: MessagesViewModel) {
     val messages by viewModel.messages.collectAsStateWithLifecycle()
     val expandedMesssagesIds by viewModel.expandedMessagesIdsList.collectAsStateWithLifecycle()
+    var textValue = remember { mutableStateOf("") }
+    var textChanged = remember { mutableStateOf(false) }
     val context = LocalContext.current
     val bgColour = remember {
-        Color(ContextCompat.getColor(context, R.color.white))
+        Color(ContextCompat.getColor(context, androidx.appcompat.R.color.material_grey_300))
     }
 
     Scaffold(backgroundColor = bgColour) { paddingValues ->
-        LazyColumn(
+        Column(
             Modifier
-                .padding(paddingValues)
-                .fillMaxWidth()
-                .wrapContentHeight(),
+                .fillMaxSize(),
+            //verticalArrangement = Arrangement.SpaceBetween
         ) {
-            items(messages!!, MessagesModel::id) { message ->
-
-                var arragement: Arrangement.Horizontal =
-                    if(message.fromId == ssm.fetch(ssm.USER_ID)?.toInt()){
-                    Arrangement.End
-                } else{
-                    Arrangement.Start
+            LazyColumn(
+                Modifier
+                    .padding(paddingValues)
+                    .fillMaxWidth()
+            ) {
+                stickyHeader {
+                    Text(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        textAlign = TextAlign.Center,
+                        text = "Чат обращения",
+                    )
                 }
-                MessageBubble(Modifier, arragement, message)
+                items(messages!!, MessagesModel::id) { message ->
+
+                    var arragement: Arrangement.Horizontal =
+                        if (message.fromId == ssm.fetch(ssm.USER_ID)?.toInt()) {
+                            Arrangement.End
+                        } else {
+                            Arrangement.Start
+                        }
+                    MessageBubble(Modifier, arragement, message)
+                }
             }
+            TextField(
+                textValue.value,
+                {
+                    textValue.value = it
+                    textChanged.value = true
+                },
+                Modifier
+                    //.defaultMinSize(minHeight = 100.dp)
+                    .requiredHeightIn(min = 130.dp)
+                    .padding(start = 10.dp, bottom = 10.dp)
+                )
         }
     }
 }
@@ -92,6 +128,7 @@ private fun MessageBubble(
     allignment: Arrangement.Horizontal,
     message: MessagesModel
 ) {
+    val bubleColour = MaterialTheme.colorScheme.primary
     Row(
         modifier = Modifier
             .fillMaxWidth()
@@ -105,11 +142,12 @@ private fun MessageBubble(
                     .widthIn(max = 220.dp)
                     .drawBehind {
                         drawRoundRect(
-                            Color(0xFFBBAAEE),
+                            color = bubleColour,
                             cornerRadius = CornerRadius(10.dp.toPx())
                         )
                     }
                     .padding(6.dp),
+                color = Color.White,
                 text = message.messageText!!,
                 fontSize = 18.sp
             )
