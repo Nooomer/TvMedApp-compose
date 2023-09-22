045ebcc77fe5cdda7ab3f43563f45db6a56daea8
Nooomer <ampilogov.qaz@gmail.com>
2023-09-22 23:06:04 +0500

    Change color

--- app/src/main/java/ru/nooomer/tvmedapp_compose/ui/theme/Color.kt
+++ app/src/main/java/ru/nooomer/tvmedapp_compose/ui/theme/Color.kt
@@ -2,7 +2,7 @@ package ru.nooomer.tvmedapp_compose.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
+val Purple80 = Color(0xFF4f378b)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
 
--- app/src/main/java/ru/nooomer/tvmedapp_compose/ui/theme/Theme.kt
+++ app/src/main/java/ru/nooomer/tvmedapp_compose/ui/theme/Theme.kt
@@ -20,22 +20,21 @@ import androidx.core.view.WindowCompat
 
 val cardCollapsedBackgroundColor = Color(0xFFFEFFFD)
 val cardExpandedBackgroundColor = Color(0xFFFFDA6D)
-public var textFieldColor: TextFieldColors? = null
+var textFieldColor: TextFieldColors? = null
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80,
-    background = Color.LightGray
+	primary = Purple80,
+	secondary = PurpleGrey80,
+	tertiary = Pink80,
+	background = Color.Black
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40,
-    background = Color.LightGray
-    /* Other default colors to override
-surface = Color(0xFFFFFBFE),
+	primary = Purple40,
+	secondary = PurpleGrey40,
+	tertiary = Pink40,
+	background = Color.LightGray
+	/* Other default colors to override
 onPrimary = Color.White,
 onSecondary = Color.White,
 onTertiary = Color.White,
@@ -46,47 +45,47 @@ onSurface = Color(0xFF1C1B1F),
 
 @Composable
 fun TvMedApp_composeTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
-    content: @Composable () -> Unit
+	darkTheme: Boolean = isSystemInDarkTheme(),
+	// Dynamic color is available on Android 12+
+	dynamicColor: Boolean = true,
+	content: @Composable () -> Unit
 ) {
-    textFieldColor = TextFieldDefaults.colors(
-        focusedTextColor = Color.Black,
-        unfocusedTextColor = Color.Black,
-        errorTextColor = Color.Black,
-        focusedContainerColor = Color.LightGray,
-        unfocusedContainerColor = Color.LightGray,
-        disabledContainerColor = Color.LightGray,
-        errorContainerColor = Color.LightGray,
-        errorCursorColor = Color.Red,
-        focusedIndicatorColor = Color.Transparent,
-        unfocusedIndicatorColor = Color.Transparent,
-        disabledIndicatorColor = Color.Transparent,
-        errorIndicatorColor = Color.Transparent,
-        errorLabelColor = Color.Red,
-    )
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
+	textFieldColor = TextFieldDefaults.colors(
+		focusedTextColor = Color.Black,
+		unfocusedTextColor = Color.Black,
+		errorTextColor = Color.Black,
+		focusedContainerColor = Color.LightGray,
+		unfocusedContainerColor = Color.LightGray,
+		disabledContainerColor = Color.LightGray,
+		errorContainerColor = Color.LightGray,
+		errorCursorColor = Color.Red,
+		focusedIndicatorColor = Color.Transparent,
+		unfocusedIndicatorColor = Color.Transparent,
+		disabledIndicatorColor = Color.Transparent,
+		errorIndicatorColor = Color.Transparent,
+		errorLabelColor = Color.Red,
+	)
+	val colorScheme = when {
+		dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+			val context = LocalContext.current
+			if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+		}
 
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
-    val view = LocalView.current
-    if (!view.isInEditMode) {
-        SideEffect {
-            val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
-        }
-    }
+		darkTheme -> DarkColorScheme
+		else -> LightColorScheme
+	}
+	val view = LocalView.current
+	if (!view.isInEditMode) {
+		SideEffect {
+			val window = (view.context as Activity).window
+			window.statusBarColor = colorScheme.primary.toArgb()
+			WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+		}
+	}
 
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
-        content = content
-    )
+	MaterialTheme(
+		colorScheme = colorScheme,
+		typography = Typography,
+		content = content
+	)
 }
