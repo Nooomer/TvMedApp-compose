13b1ae85738670379868fafebfe431e3d9c54ec7
Nooomer <ampilogov.qaz@gmail.com>
2023-09-24 20:29:47 +0500

    Add api for chat

--- app/src/main/java/ru/nooomer/tvmedapp_compose/api/API.kt
+++ app/src/main/java/ru/nooomer/tvmedapp_compose/api/API.kt
@@ -9,39 +9,44 @@ import ru.nooomer.tvmedapp_compose.api.interfaces.TreatmentApi
 import ru.nooomer.tvmedapp_compose.api.interfaces.UserAuthApi
 import ru.nooomer.tvmedapp_compose.api.models.ChatDto
 import ru.nooomer.tvmedapp_compose.api.models.LoginData
+import ru.nooomer.tvmedapp_compose.api.models.NewMessageDto
 import ru.nooomer.tvmedapp_compose.api.models.SymptomDto
 import ru.nooomer.tvmedapp_compose.api.models.TreatmentDto
 import java.util.UUID
 
 object API : KoinComponent {
-    private val common by inject<Common>()
-    private val authService = common.initRetrofit<UserAuthApi>()
-    private val treatmentService = common.initRetrofit<TreatmentApi>()
-    private val symptomsService = common.initRetrofit<SymptomApi>()
-    private val chatService = common.initRetrofit<ChatApi>()
-    fun login(login: String, password: String): Any {
-        return authService.login(LoginData(login, password)).execute().body()!!
-    }
+	private val common by inject<Common>()
+	private val authApiService = common.initRetrofit<UserAuthApi>()
+	private val treatmentApiService = common.initRetrofit<TreatmentApi>()
+	private val symptomsApiService = common.initRetrofit<SymptomApi>()
+	private val chatApiService = common.initRetrofit<ChatApi>()
+	fun login(login: String, password: String): Any {
+		return authApiService.login(LoginData(login, password)).execute().body()!!
+	}
 
-    fun checkLogin(): Boolean {
-        return authService.checkLogin().execute().body()!!
-    }
+	fun checkLogin(): Boolean {
+		return authApiService.checkLogin().execute().body()!!
+	}
 
-    fun getTreatmentForUser(): Any? {
-        val result = treatmentService.getTreatment().execute()
-        return if (!result.isSuccessful) {
-            //result.errorBody() as ErrorDto?
-            listOf<TreatmentDto>()
-        } else {
-            result.body()
-        }
-    }
+	fun getTreatmentForUser(): Any? {
+		val result = treatmentApiService.getTreatment().execute()
+		return if (!result.isSuccessful) {
+			//result.errorBody() as ErrorDto?
+			listOf<TreatmentDto>()
+		} else {
+			result.body()
+		}
+	}
 
-    fun getSymptoms(): List<SymptomDto>? {
-        return symptomsService.getSymptoms().execute().body()
-    }
+	fun getSymptoms(): List<SymptomDto>? {
+		return symptomsApiService.getSymptoms().execute().body()
+	}
 
-    fun getChat(treatmentId: UUID): ChatDto? {
-        return chatService.getChat(treatmentId).execute().body()
-    }
+	fun getChat(treatmentId: UUID): ChatDto? {
+		return chatApiService.getChat(treatmentId).execute().body()
+	}
+
+	fun sendMessage(treatmentId: UUID, newMessageDto: NewMessageDto): ChatDto? {
+		return chatApiService.sendMessage(treatmentId, newMessageDto).execute().body()
+	}
 }
--- app/src/main/java/ru/nooomer/tvmedapp_compose/api/interfaces/ChatApi.kt
+++ app/src/main/java/ru/nooomer/tvmedapp_compose/api/interfaces/ChatApi.kt
@@ -1,12 +1,21 @@
 package ru.nooomer.tvmedapp_compose.api.interfaces
 
 import retrofit2.Call
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Path
 import ru.nooomer.tvmedapp_compose.api.models.ChatDto
+import ru.nooomer.tvmedapp_compose.api.models.NewMessageDto
 import java.util.UUID
 
 interface ChatApi {
-    @GET("treatment/chat/{treatment-id}")
-    fun getChat(@Path("treatment-id") treatmentId: UUID?): Call<ChatDto>
+	@GET("treatment/chat/{treatment-id}")
+	fun getChat(@Path("treatment-id") treatmentId: UUID?): Call<ChatDto>
+
+	@POST("treatment/chat/{treatment-id}")
+	fun sendMessage(
+		@Path("treatment-id") treatmentId: UUID?,
+		@Body newMessageDto: NewMessageDto
+	): Call<ChatDto>
 }
